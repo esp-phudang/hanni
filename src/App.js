dfd468dd8b9f07ceb7dce2ae25603764c3a59f82
esp-phudang <phu.dang@eastplayers.io>
2021-03-02 22:05:38 +0700

    conflict

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hu h uh u huh u
+          Learn React
+          conflict
         </a>
       </header>
     </div>
